6934bb3030f5ee58cbf031cc9dc3788c2afc9b42
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2021-10-20 21:58:15 +0530

    Update App.js
    Ui for 3rd App is created

--- spanishNumber/App.js
+++ spanishNumber/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import {Text, View, ScrollView, Image, StyleSheet} from 'react-native';
+import {
+  Text, 
+  View,
+  ScrollView,
+  Image, 
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import Sound from 'react-native-sound';
 
 const soundList = [
@@ -17,13 +24,51 @@ const soundList = [
 
 const App = () => {
   return (
-    <ScrollView>
-      <Image source={require('./assests/logo.png')} />
-      <View>
-      
+    <ScrollView style={Styles.container}>
+      <Image source={require('./assests/logo.png')} style={Styles.logo} />
+      <View style={Styles.gridContainer}>
+      {soundList.map((Sound) =>(
+        <TouchableOpacity key={Sound}  style={Styles.box}>
+        <Text style={Styles.text}>{Sound}</Text>
+        </TouchableOpacity>
+      ))}
       </View>
     </ScrollView>
   );
 };
 
 export default App;
+
+const Styles = StyleSheet.create({
+  container:{
+    flex : 1,
+    backgroundColor : '#1b262c'
+  },
+  logo:{
+    alignSelf: 'center',
+    marginTop: 15,
+  },
+  gridContainer:{
+    flex:1,
+    margin: 5,
+    flexDirection:'row',
+    flexWrap:"wrap",
+    alignItems:"flex-start",
+    justifyContent:"space-around"
+},
+  box:{
+    height:110,
+    alignItems:"center",
+    justifyContent:"center",
+    width:"46%",
+    marginVertical:6,
+    backgroundColor:"#0f4c75",
+    borderRadius:5,
+    shadowColor:"#393e46",
+    elevation:5,
+  },
+  text:{
+    fontSize: 50,
+    color:'#ff4301',
+  }
+})
